17bc63207eff876f7362fa681b0a99c3cc8234f6
EC2 Default User <ec2-user@ip-172-31-16-146.us-west-2.compute.internal>
2017-11-16 16:37:12 +0000

    training and eval scripts for running while removing some sv variables; remakes and loads weights into model instead of saving and loading model directly

--- Train/train_deepdoubleb_sv_removals.py
+++ Train/train_deepdoubleb_sv_removals.py
@@ -9,6 +9,7 @@ import keras
 #keras.backend.set_image_data_format('channels_last')
 
 
+
 # In[2]:
 
 class MyClass:
@@ -28,32 +29,37 @@ import sys
 
 args = MyClass()
 args.inputDataCollection = '/cms-sc17/convert_20170717_ak8_deepDoubleB_db_sv_train_val/dataCollection.dc'
-args.outputDir = 'train_deep_sv_removals_ptrel_erel_pt_mass'
+args.outputDir = 'train_deep_sv_removals_test'
 
-#also does all the parsing
+
+TrainBool = True
+EvalBool= True
+
 train=training_base(testrun=False,args=args)
 
 
-if not train.modelSet():
-    from DeepJet_models_removals import deep_model_removal_sv as model
+if TrainBool:
+    #also does all the parsing
 
-    train.setModel(model)
+    if not train.modelSet():
+        from DeepJet_models_removals import deep_model_removal_sv as model
+        from DeepJet_models_removals import Slicer1D
+
+        train.setModel(model)
     
-    train.compileModel(learningrate=0.001,
-                       loss=['categorical_crossentropy'],
-                       metrics=['accuracy'])
+        train.compileModel(learningrate=0.001,
+                           loss=['categorical_crossentropy'],
+                           metrics=['accuracy'])
     
 
-model,history,callbacks = train.trainModel(nepochs=500, 
-                                batchsize=1024, 
-                                 stop_patience=1000, 
-                                 lr_factor=0.8, 
-                                 lr_patience=10, 
-                                 lr_epsilon=0.00000001, 
-                                 lr_cooldown=2, 
-                                 lr_minimum=0.00000001, 
-                                           maxqsize=100)
-
-
+        model,history,callbacks = train.trainModel(nepochs=2, 
+                                                   batchsize=1024, 
+                                                   stop_patience=1000, 
+                                                   lr_factor=0.8, 
+                                                   lr_patience=10, 
+                                                   lr_epsilon=0.00000001, 
+                                                   lr_cooldown=2, 
+                                                   lr_minimum=0.00000001, 
+                                                   maxqsize=100)
 
 
